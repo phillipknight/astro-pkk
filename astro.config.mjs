aebad8c8c477114b5849c20040812723ed4d7517
phillipknight <phillipkknight@gmail.com>
2024-03-05 16:10:22 -0500

    Blog Tutorial Completion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "http://pkk-astro.netlify.app/"
+  site: "http://pkk-astro.netlify.app/",
+  integrations: [preact()]
 });
