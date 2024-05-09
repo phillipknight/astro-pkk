d33c62d1333cb91de9b4377e0c096f410cc19f0b
phillipknight <phillipkknight@gmail.com>
2024-05-09 18:44:04 -0400

    Global: remove Astro Icon from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 
-import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://pkk-astro.netlify.app/",
-  integrations: [preact(), icon()]
+  integrations: [preact(),]
 });
